1f29f4d585a157a0b91aad255f086fe0606debd8
maisy <maisy@test.com>
2020-12-12 19:32:45 +0800

    初始项目

---
+++ mt-demo02/src/test/java/com/msy/mtdemo02/MtDemo02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msy.mtdemo02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtDemo02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ mt-zuul/src/test/java/com/msy/mtzuul/MtZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msy.mtzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
